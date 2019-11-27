afc57b5e1e6bdf876e5f6f2f2d1ecb231acb57fe
andy <wudi198@gmail.com>
2019-11-26 17:08:52 -0800

    daily bk

--- include/tsne.hpp
+++ include/tsne.hpp
@@ -13,45 +13,63 @@ template<typename T>
 class TSNE{
 
     protected:
-    const ushort dim;
-    size_t n_total;
+
+    ushort x_dim;
+    ushort y_dim;
     VpTree<T> *vp_tree;
-    BarnesHutTree<T> *bh_tree;
+    std::vector<T*> X;
+    std::vector<T*> Y;
+    T* X_mean;
 
     struct Matrix{
         size_t n_rows;
         size_t n_cols;
         T *vals;
+        size_t *indices;
         bool is_symmtric;
 
-        Matrix():n_rows(0),n_cols(0), is_symmtric(false){}
+        Matrix():n_rows(0),n_cols(0), is_symmtric(false), vals(nullptr), indices(nullptr){}
         Matrix(size_t n_rows, size_t n_cols): n_rows(n_rows), n_cols(n_cols),is_symmtric(false){
            vals = new T[n_rows * n_cols];
+           indices = new size_t[n_rows * n_cols];
+        }
+        ~Matrix(){
+            delete vals;
+            delete indices;
         }
 
         T* get(size_t row_i, size_t col_j);
-        void set(size_t row_i, size_t col_j, T v);
+        void set(size_t row_i, size_t col_j, size_t idx, T v);
         void makeSymmtric();
     };
 
+    void initX(size_t n, T *x);
     void computeGradient();
-    void computeGaussianPerplexity(size_t n, T perplexity, Matrix &mat);
+    void computeGaussianPerplexity(size_t n, T perplexity, T *x, Matrix &mat);
+    void computeGaussianPerplexity(size_t n, size_t k, T perplexity, T *x, Matrix &mat);
+
+    void zeroMean(size_t n, ushort d, T *mean, T **inp);
 
     public:
 
-    TSNE():dim(0), n_total(0), vp_tree(nullptr), bh_tree(nullptr){}
+    TSNE():x_dim(0), y_dim(0), vp_tree(nullptr), X_mean(nullptr){}
 
-    explicit TSNE(const ushort dim): dim(dim), n_total(0), vp_tree(nullptr), bh_tree(nullptr){}
+    explicit TSNE(ushort x_dim, ushort y_dim): x_dim(x_dim), y_dim(y_dim), vp_tree(nullptr), X_mean(nullptr){}
 
-    TSNE(size_t n, ushort dim, T *data);
+    ~TSNE(){
+        delete vp_tree;
+        for(auto iter = X.begin(); iter != X.end(); iter++){
+            delete (*iter);
+        }
+        for(auto iter = Y.begin(); iter != Y.end(); iter++){
+            delete (*iter);
+        }
+    }
 
-    void run(T* y, T perplexity, T theta, int rand_seed,
+    void run(size_t n, T *x, T* y, T perplexity, T theta, int rand_seed,
              bool skip_random_init, int max_iter, int stop_lying_iter, int mom_switch_iter);
 
-    void run(size_t xn, T *x, T* y, T perplexity, T theta, int rand_seed,
-             bool skip_random_init, int max_iter, int stop_lying_iter, int mom_switch_iter);
-
-
+    void reIndex();
 
 };
 
--- src/tsne.cpp
+++ src/tsne.cpp
@@ -2,12 +2,75 @@
 // Created by andy on 2019-11-25.
 //
 
+#include <cstring>
 #include "tsne.hpp"
 
 namespace tsne{
 
+
     template<typename T>
-    TSNE<T>::TSNE(size_t n, ushort dim, T *data): TSNE(dim){
+    void TSNE<T>::run(size_t n, T *x, T *y, T perplexity, T theta, int rand_seed, bool skip_random_init, int max_iter,
+                      int stop_lying_iter, int mom_switch_iter) {
+        if(X.empty()){
+            initX(n, x);
+        }
+        else{
+            for(size_t i = 0; i < n; i ++){
+                T *cx = new T[x_dim];
+                memcpy(cx, x + i*x_dim, sizeof(T)*x_dim);
+                X.push_back(cx);
+            }
+            zeroMean(n, x_dim, X_mean, X.data() + (X.size() - n));
+        }
+        vp_tree = new VpTree<T>(n, x_dim, x);
+
+    }
+
+    template<typename T>
+    void TSNE<T>::initX(size_t n, T *x) {
+        delete X_mean;
+        X_mean = (T*)calloc(x_dim, sizeof(T));
+        for(size_t i = 0; i < n; i ++){
+            T *cx = new T[x_dim];
+            memcpy(cx, x + i*x_dim, sizeof(T)*x_dim);
+            X.push_back(cx);
+            for(ushort d = 0; d < x_dim; d++){
+                X_mean[d] += cx[d];
+            }
+        }
+        for(ushort d = 0; d < x_dim; d++) X_mean[d] /= (T)n;
+        zeroMean(n, x_dim, X_mean, X.data());
+    }
+
+    template<typename T>
+    void TSNE<T>::zeroMean(size_t n, ushort dim, T *mean, T **inps) {
+        for(size_t i = 0; i < n; i++){
+            for(ushort d = 0; d < dim; d++){
+                inps[i][d] -= mean[d];
+            }
+        }
+    }
+
+
+    template<typename T>
+    void TSNE<T>::computeGaussianPerplexity(size_t n, T perplexity, T *x, tsne::TSNE<T>::Matrix &mat) {
+
+    }
+
+    template<typename T>
+    void TSNE<T>::computeGaussianPerplexity(size_t n, size_t k, T perplexity, T *x, tsne::TSNE<T>::Matrix &mat) {
+        if(k == n) computeGaussianPerplexity(n, perplexity, x, mat);
+
+        for(size_t i = 0; i < n; i++){
+            std::vector<T> dists(k);
+            std::vector<size_t> indices(k);
+            vp_tree->search(x + i * x_dim, k, indices, dists);
+        }
+
+    }
+
+    template<typename T>
+    void TSNE<T>::computeGradient() {
 
     }
 
@@ -18,32 +81,18 @@ namespace tsne{
 
 
     template <typename T>
-    void TSNE<T>::Matrix::set(size_t row_i, size_t col_j, T v) {
+    void TSNE<T>::Matrix::set(size_t row_i, size_t col_j, size_t idx, T v) {
+        vals[row_i * n_cols + col_j] = v;
+        indices[row_i * n_cols + col_j] = idx;
+    }
+
+    template <typename T>
+    void TSNE<T>::Matrix::makeSymmtric() {
 
     }
 
-//    template<typename T>
-//    class TSNE<T>::SymmetricMatrix{
-//        size_t n;
-//        T *val;
-//        size_t *offset;
-//
-//        SymmetricMatrix():n(0), val(nullptr), offset(nullptr){}
-//        explicit SymmetricMatrix(size_t n): SymmetricMatrix(){
-//            this->n = n;
-//        }
-//
-//        ~SymmetricMatrix(){
-//            delete val;
-//            delete offset;
-//        }
-//
-//        T* get(size_t row_i, size_t col_j){
-//            return val + offset[row_i] + col_j;
-//        }
-//
-//    };
 
-
+    template class TSNE<float>;
+    template class TSNE<double>;
 
 }
